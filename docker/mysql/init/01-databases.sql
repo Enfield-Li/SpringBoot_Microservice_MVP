60415099ab0730303e5a8386652acb224e744db9
Enfield-Li <1076462153@qq.com>
2022-08-24 07:08:22 +0800

    No need to create root user

--- docker/mysql/init/01-databases.sql
+++ docker/mysql/init/01-databases.sql
@@ -3,6 +3,5 @@ CREATE DATABASE IF NOT EXISTS `spring_microservices_customer`;
 CREATE DATABASE IF NOT EXISTS `spring_microservices_fraud`;
 
 -- create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
 GRANT ALL ON `spring_microservices_customer`.* TO 'root'@'%';
 GRANT ALL ON `spring_microservices_fraud`.* TO 'root'@'%';
