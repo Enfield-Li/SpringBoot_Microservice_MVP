2a7947db349387e805089cf63703dc17bb6fb107
Enfield-Li <1076462153@qq.com>
2022-08-24 04:32:40 +0800

    Done

--- customer/src/main/java/com/example/customer/CustomerApplication.java
+++ customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -2,11 +2,28 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class CustomerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(CustomerApplication.class, args);
+    ConfigurableApplicationContext ctx = SpringApplication.run(
+      CustomerApplication.class,
+      args
+    );
+
+    log.info("Starts up");
+
+    Customer customer = new Customer();
+    customer.setEmail("email");
+    customer.setUsername("username");
+
+    CustomerRepository repo = ctx.getBean(CustomerRepository.class);
+    Customer c = repo.save(customer);
+    log.info("saved: " + c.toString());
   }
 }
