ccfae1522614ab283d4c806d2daee63b252d7297
Enfield-Li <1076462153@qq.com>
2022-09-01 16:44:46 +0800

    rabbitmq

---
+++ clients/src/main/java/com/example/clients/notification/NotificationRequest.java
@@ -0,0 +1,15 @@
+package com.example.clients.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationRequest {
+
+  Integer toCustomerId;
+  String toCustomerName;
+  String message;
+}
