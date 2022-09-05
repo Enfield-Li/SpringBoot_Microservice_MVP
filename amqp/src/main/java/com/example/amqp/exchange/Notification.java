439a508ebae3538e1ea8c45b7d2d2dc20bafe7f4
Enfield-Li <1076462153@qq.com>
2022-09-05 11:36:18 +0800

    Clean up

--- amqp/src/main/java/com/example/amqp/exchange/Notification.java
+++ amqp/src/main/java/com/example/amqp/exchange/Notification.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Notification {
 
-  // @Value("${rabbitmq.exchanges.internal}")
   public static final String internalExchange = "internal.exchange";
 
-  // @Value("${rabbitmq.queues.notification}")
   public static final String notificationQueue = "notification.queue";
 
-  // @Value("${rabbitmq.routing-keys.internal-notification}")
   public static final String notificationRoutingKey =
     "internal.notification.routing-key";
 }
